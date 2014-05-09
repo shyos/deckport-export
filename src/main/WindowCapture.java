1dd3802eeffc29acab5805bc5675ddf114bbc84c
Shyos <Shyos>
2014-05-09 11:37:45 +0300

    Window Capture Added

---
+++ src/main/WindowCapture.java
@@ -0,0 +1,143 @@
+package main;
+import java.awt.Graphics;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+import jna.extra.GDI32Extra;
+import jna.extra.User32Extra;
+import jna.extra.WinGDIExtra;
+
+import com.sun.jna.Memory;
+import com.sun.jna.platform.win32.GDI32;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef.HBITMAP;
+import com.sun.jna.platform.win32.WinDef.HDC;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinDef.RECT;
+import com.sun.jna.platform.win32.WinGDI;
+import com.sun.jna.platform.win32.WinGDI.BITMAPINFO;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+
+public class WindowCapture extends JFrame {
+
+	private static final int ITERATIONS_FOR_MINIMISE = 8;
+	private HWND _windowHandle = null;
+	private String _windowHandleId = null;
+	private boolean isFullscreen = false;
+	private boolean isMinimised = false;
+    private int minimisedCount = 0;
+	private boolean _isFullScreen(Rectangle rect) {
+		// check to make sure Hearthstone's not in full screen
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int width = gd.getDisplayMode().getWidth();
+		int height = gd.getDisplayMode().getHeight();
+		return (rect.width >= width && rect.height >= height);
+	}
+    public BufferedImage capture(HWND hWnd) {
+
+    	HDC hdcWindow = User32.INSTANCE.GetDC(hWnd);
+		HDC hdcMemDC = GDI32.INSTANCE.CreateCompatibleDC(hdcWindow);
+
+		RECT bounds = new RECT();
+		User32Extra.INSTANCE.GetClientRect(hWnd, bounds);
+
+		// check to make sure the window's not minimized
+		if(bounds.toRectangle().width >= 1024) {
+			if(isMinimised) {
+				System.out.println("Hearthstone window restored");
+				isMinimised = false;
+			}
+
+			if(_isFullScreen(bounds.toRectangle())) {
+				if(!isFullscreen) {
+					System.out.println("Hearthstone running in fullscreen");
+					isFullscreen = true;
+				}
+				return null;
+			} else {
+				int width = bounds.right - bounds.left;
+				int height = bounds.bottom - bounds.top;
+
+				HBITMAP hBitmap = GDI32.INSTANCE.CreateCompatibleBitmap(hdcWindow, width, height);
+
+				HANDLE hOld = GDI32.INSTANCE.SelectObject(hdcMemDC, hBitmap);
+				GDI32Extra.INSTANCE.BitBlt(hdcMemDC, 0, 0, width, height, hdcWindow, 0, 0, WinGDIExtra.SRCCOPY);
+
+				GDI32.INSTANCE.SelectObject(hdcMemDC, hOld);
+				GDI32.INSTANCE.DeleteDC(hdcMemDC);
+
+				BITMAPINFO bmi = new BITMAPINFO();
+				bmi.bmiHeader.biWidth = width;
+				bmi.bmiHeader.biHeight = -height;
+				bmi.bmiHeader.biPlanes = 1;
+				bmi.bmiHeader.biBitCount = 32;
+				bmi.bmiHeader.biCompression = WinGDI.BI_RGB;
+
+				Memory buffer = new Memory(width * height * 4);
+				GDI32.INSTANCE.GetDIBits(hdcWindow, hBitmap, 0, height, buffer, bmi, WinGDI.DIB_RGB_COLORS);
+
+				BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+				image.setRGB(0, 0, width, height, buffer.getIntArray(0, width * height), 0, width);
+
+				GDI32.INSTANCE.DeleteObject(hBitmap);
+				User32.INSTANCE.ReleaseDC(hWnd, hdcWindow);
+				return image;
+			}
+		}
+		if (!isMinimised) {
+            // Hearthstone has brief periods where its window is not displayed, such as during startup and when changing
+            // scree size. We don't want to show a warning for these, so we wait a couple of iterations before assuming
+            // that the window has been minimised.
+            if (minimisedCount < ITERATIONS_FOR_MINIMISE) {
+                minimisedCount++;
+            } else {
+            	System.out.println("Warning! Hearthstone minimized. No detection possible.");
+                isMinimised = true;
+                minimisedCount = 0;
+            }
+		}
+		return null;
+
+    }
+
+    public static void main(String[] args) {
+        new WindowCapture();
+    }
+
+    BufferedImage image;
+
+    public WindowCapture() {
+        HWND hWnd = User32.INSTANCE.FindWindow(null, "Hearthstone");
+        this.image = capture(hWnd);
+/*
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setExtendedState(MAXIMIZED_BOTH);
+        setVisible(true);*/
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(image, 20, 40, null);
+    }
+	/**
+	 * @return the image
+	 */
+	public BufferedImage getImage() {
+		return image;
+	}
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+
+
+}
