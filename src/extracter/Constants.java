0644b9d1c7cd8f32ad4b9738c82d7decbe77f85f
Shyos <shalidyildiz@gmail.com>
2014-06-01 19:46:10 +0300

    v0.2.0.beta

--- src/extracter/Constants.java
+++ src/extracter/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 	public static int GUICardOrder = 0;
 	public static Resolution _RESOLUTION = new Resolution(1024,768);
 	public static int isBig = 1;
-	public static String version = "v0.1.0.alpha";
+	public static String version = "v0.2.0.beta";
 	public static int probItemLimit = 5;
 }
